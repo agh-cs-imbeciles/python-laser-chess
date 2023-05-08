eb3aa962fac7aafee3b2905a1114f6507d127d01
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-08 23:55:36 +0200

    Init `backend` project

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+import asyncio
+import websockets
+
+
+async def handler(websocket):
+    while True:
+        try:
+            message = await websocket.recv()
+        except websockets.ConnectionClosedOK:
+            break
+        print(message)
+
+
+async def main():
+    async with websockets.serve(handler, "", 8000):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
