4f665c7b99dab295d45280ab34719078a412f4c0
Jakub Szaredko <jakub.szaredko@gmail.com>
2023-05-19 22:45:51 +0200

    Resize the board to rectangle, change background cells

--- src/app/gui/constants.py
+++ src/app/gui/constants.py
@@ -6,3 +6,4 @@ class Path:
     CURRENT_PATH: str = pathlib.Path(__file__).parent.resolve()
     IMG_PATH: str = os.path.join(CURRENT_PATH, "../assets/images")
     PIECE_IMG_PATH: str = os.path.join(CURRENT_PATH, "../assets/images/board/pixel/wooden/pieces")
+    WOODEN_IMG_PATH: str = os.path.join(CURRENT_PATH, "../assets/images/board/pixel/wooden")
